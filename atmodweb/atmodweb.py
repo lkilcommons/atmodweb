d52bcb7fd47bda047dae22f4f63b991e4482a9c8
lkilcommons <liamk@nut.colorado.edu>
2016-02-11 16:47:07 -0700

    Added new login message

--- atmodweb/atmodweb.py
+++ atmodweb/atmodweb.py
@@ -42,6 +42,16 @@ ch.setFormatter(formatter)
 #log.addHandler(fh)
 log.addHandler(ch)
 
+def strongly_expire(func):
+    """Decorator that sends headers that instruct browsers and proxies not to cache.
+    """
+    def newfunc(*args, **kwargs):
+        cherrypy.response.headers['Expires'] = 'Sun, 19 Nov 1978 05:00:00 GMT'
+        cherrypy.response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+        cherrypy.response.headers['Pragma'] = 'no-cache' 
+        return func(*args, **kwargs)
+    return newfunc
+
 class ansicolors:
 	HEADER = '\033[95m'
 	OKBLUE = '\033[94m'
